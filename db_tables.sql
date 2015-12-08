f8c34f09fe89d3c3f8f434368418bbfb8a24c3de
RomaKoks <79817815081@yandex.ru>
2015-12-08 18:26:08 +0300

    добавил таблицу account ну и все процедуры и триггеры, что были на данный момент тепреь в procedures.sql и db_triggers.sql соответственно

--- db_tables.sql
+++ db_tables.sql
@@ -5,19 +5,7 @@ COLLATE utf8_general_ci;
 
 USE new_sevryukov_task;
 
-CREATE TABLE IF NOT EXISTS subjects (
-  id char(36) NOT NULL DEFAULT '1',
-  type enum ('EMPLOYEE','CLIENT','COMPANY','SHOP','FOREMAN','MANAGER') NOT NULL,
-  name varchar(200) NOT NULL,
-  username varchar(200) DEFAULT NULL,
-  PRIMARY KEY (id)
-)
-ENGINE = INNODB
-AVG_ROW_LENGTH = 2048
-CHARACTER SET utf8
-COLLATE utf8_general_ci;
-
-CREATE TABLE IF NOT EXISTS log (
+CREATE TABLE IF NOT EXISTS new_sevryukov_task.log (
   id char(36) NOT NULL,
   date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   description text NOT NULL,
@@ -28,33 +16,42 @@ AVG_ROW_LENGTH = 963
 CHARACTER SET utf8
 COLLATE utf8_general_ci;
 
-CREATE TABLE IF NOT EXISTS balance_per_day (
+CREATE TABLE IF NOT EXISTS new_sevryukov_task.subjects (
   id char(36) NOT NULL DEFAULT '1',
-  subject_id char(36) NOT NULL,
-  date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  balance decimal(10, 2) NOT NULL,
-  PRIMARY KEY (id),
-  CONSTRAINT Balance_subjects FOREIGN KEY (subject_id)
-  REFERENCES subjects (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  type enum ('EMPLOYEE', 'CLIENT', 'COMPANY', 'SHOP', 'FOREMAN', 'MANAGER') NOT NULL,
+  name varchar(200) NOT NULL,
+  username varchar(200) DEFAULT NULL,
+  PRIMARY KEY (id)
 )
 ENGINE = INNODB
-AVG_ROW_LENGTH = 1024
+AVG_ROW_LENGTH = 2048
 CHARACTER SET utf8
 COLLATE utf8_general_ci;
 
-CREATE TABLE IF NOT EXISTS objects (
+CREATE TABLE IF NOT EXISTS new_sevryukov_task.account (
+  id char(32) NOT NULL DEFAULT '1',
+  subject_id char(32) DEFAULT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT FK_account_subjects_id FOREIGN KEY (subject_id)
+  REFERENCES new_sevryukov_task.subjects (id) ON DELETE NO ACTION ON UPDATE NO ACTION
+)
+ENGINE = INNODB
+CHARACTER SET utf8
+COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS new_sevryukov_task.objects (
   id char(36) NOT NULL,
   subject_id char(36) NOT NULL,
   description text NOT NULL,
   PRIMARY KEY (id),
   CONSTRAINT objects_subjects FOREIGN KEY (subject_id)
-  REFERENCES subjects (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  REFERENCES new_sevryukov_task.subjects (id) ON DELETE RESTRICT ON UPDATE RESTRICT
 )
 ENGINE = INNODB
 CHARACTER SET utf8
 COLLATE utf8_general_ci;
 
-CREATE TABLE IF NOT EXISTS tickets (
+CREATE TABLE IF NOT EXISTS new_sevryukov_task.tickets (
   id char(36) NOT NULL,
   subject_id char(36) NOT NULL,
   description text NOT NULL,
@@ -63,87 +60,101 @@ CREATE TABLE IF NOT EXISTS tickets (
   reason text DEFAULT NULL,
   PRIMARY KEY (id),
   CONSTRAINT tickets_subjects FOREIGN KEY (subject_id)
-  REFERENCES subjects (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  REFERENCES new_sevryukov_task.subjects (id) ON DELETE RESTRICT ON UPDATE RESTRICT
 )
 ENGINE = INNODB
 CHARACTER SET utf8
 COLLATE utf8_general_ci;
 
-CREATE TABLE IF NOT EXISTS transactions (
+CREATE TABLE IF NOT EXISTS new_sevryukov_task.assignments (
+  subject_id char(36) NOT NULL,
+  object_id char(36) NOT NULL,
+  CONSTRAINT assignments_objects FOREIGN KEY (object_id)
+  REFERENCES new_sevryukov_task.objects (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT assignments_subjects FOREIGN KEY (subject_id)
+  REFERENCES new_sevryukov_task.subjects (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+)
+ENGINE = INNODB
+CHARACTER SET utf8
+COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS new_sevryukov_task.balance_per_day (
   id char(36) NOT NULL DEFAULT '1',
-  from_subject_id char(36) NOT NULL,
-  to_subject_id char(36) NOT NULL,
+  account_id char(36) NOT NULL,
+  date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  balance decimal(10, 2) NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT FK_balance_per_day_account_id FOREIGN KEY (account_id)
+  REFERENCES new_sevryukov_task.account (id) ON DELETE NO ACTION ON UPDATE NO ACTION
+)
+ENGINE = INNODB
+AVG_ROW_LENGTH = 1024
+CHARACTER SET utf8
+COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS new_sevryukov_task.permissions (
+  subject_id char(36) NOT NULL,
+  permission enum ('DELETE', 'INSERT', 'READ', 'UPDATE', 'UPDATE_SELF') DEFAULT NULL,
+  object_id char(36) NOT NULL,
+  CONSTRAINT permission_objects FOREIGN KEY (object_id)
+  REFERENCES new_sevryukov_task.objects (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT permission_subjects FOREIGN KEY (subject_id)
+  REFERENCES new_sevryukov_task.subjects (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+)
+ENGINE = INNODB
+CHARACTER SET utf8
+COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS new_sevryukov_task.roles (
+  id char(36) NOT NULL,
+  subject_id char(36) NOT NULL,
+  object_id char(36) NOT NULL,
+  role enum ('FOREMAN', 'MANAGER', 'EMPLOYEE') NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT role_objects FOREIGN KEY (object_id)
+  REFERENCES new_sevryukov_task.objects (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT role_subjects FOREIGN KEY (subject_id)
+  REFERENCES new_sevryukov_task.subjects (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+)
+ENGINE = INNODB
+CHARACTER SET utf8
+COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS new_sevryukov_task.stages (
+  id char(36) NOT NULL,
+  subject_id char(36) NOT NULL,
+  object_id char(36) NOT NULL,
+  description text NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT stages_objects FOREIGN KEY (object_id)
+  REFERENCES new_sevryukov_task.objects (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT stages_subjects FOREIGN KEY (subject_id)
+  REFERENCES new_sevryukov_task.subjects (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+)
+ENGINE = INNODB
+CHARACTER SET utf8
+COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS new_sevryukov_task.transactions (
+  id char(36) NOT NULL DEFAULT '1',
+  from_account_id char(36) NOT NULL,
+  to_account_id char(36) NOT NULL,
   type enum ('SALLARY', 'MATERIAL', 'CLIENT') NOT NULL,
   value decimal(10, 2) NOT NULL DEFAULT 0.00,
   description text NOT NULL,
   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (id),
-  CONSTRAINT transactions_subjects1 FOREIGN KEY (from_subject_id)
-  REFERENCES subjects (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT transactions_subjects2 FOREIGN KEY (to_subject_id)
-  REFERENCES subjects (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  CONSTRAINT FK_transactions_from_account_id FOREIGN KEY (from_account_id)
+  REFERENCES new_sevryukov_task.account (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT FK_transactions_to_account_id FOREIGN KEY (to_account_id)
+  REFERENCES new_sevryukov_task.account (id) ON DELETE NO ACTION ON UPDATE NO ACTION
 )
 ENGINE = INNODB
 AVG_ROW_LENGTH = 2048
 CHARACTER SET utf8
 COLLATE utf8_general_ci;
 
-CREATE TABLE IF NOT EXISTS assignments (
-  subject_id char(36) NOT NULL,
-  object_id char(36) NOT NULL,
-  CONSTRAINT assignments_subjects FOREIGN KEY (subject_id)
-  REFERENCES subjects (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT assignments_objects FOREIGN KEY (object_id)
-  REFERENCES objects (id) ON DELETE RESTRICT ON UPDATE RESTRICT
-)
-ENGINE = INNODB
-CHARACTER SET utf8
-COLLATE utf8_general_ci;
-
-CREATE TABLE IF NOT EXISTS permissions (
-  subject_id char(36) NOT NULL,
-  permission enum ('DELETE', 'INSERT', 'READ', 'UPDATE', 'UPDATE_SELF') DEFAULT NULL,
-  object_id char(36) NOT NULL,
-  CONSTRAINT permission_subjects FOREIGN KEY (subject_id)
-  REFERENCES subjects (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT permission_objects FOREIGN KEY (object_id)
-  REFERENCES objects (id) ON DELETE RESTRICT ON UPDATE RESTRICT
-)
-ENGINE = INNODB
-CHARACTER SET utf8
-COLLATE utf8_general_ci;
-
-CREATE TABLE IF NOT EXISTS roles (
-  id char(36) NOT NULL,
-  subject_id char(36) NOT NULL,
-  object_id char(36) NOT NULL,
-  role enum ('FOREMAN', 'MANAGER','EMPLOYEE') NOT NULL,
-  PRIMARY KEY (id),
-  CONSTRAINT role_subjects FOREIGN KEY (subject_id)
-  REFERENCES subjects (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT role_objects FOREIGN KEY (object_id)
-  REFERENCES objects (id) ON DELETE RESTRICT ON UPDATE RESTRICT
-)
-ENGINE = INNODB
-CHARACTER SET utf8
-COLLATE utf8_general_ci;
-
-CREATE TABLE IF NOT EXISTS stages (
-  id char(36) NOT NULL,
-  subject_id char(36) NOT NULL,
-  object_id char(36) NOT NULL,
-  description text NOT NULL,
-  PRIMARY KEY (id),
-  CONSTRAINT stages_subjects FOREIGN KEY (subject_id)
-  REFERENCES subjects (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT stages_objects FOREIGN KEY (object_id)
-  REFERENCES objects (id) ON DELETE RESTRICT ON UPDATE RESTRICT
-)
-ENGINE = INNODB
-CHARACTER SET utf8
-COLLATE utf8_general_ci;
-
-CREATE TABLE IF NOT EXISTS elements (
+CREATE TABLE IF NOT EXISTS new_sevryukov_task.elements (
   id char(36) NOT NULL,
   stage_id char(36) NOT NULL,
   description text NOT NULL,
@@ -154,47 +165,9 @@ CREATE TABLE IF NOT EXISTS elements (
   completed tinyint(1) NOT NULL,
   PRIMARY KEY (id),
   CONSTRAINT elements_stages FOREIGN KEY (stage_id)
-  REFERENCES stages (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  REFERENCES new_sevryukov_task.stages (id) ON DELETE RESTRICT ON UPDATE RESTRICT
 )
 ENGINE = INNODB
 CHARACTER SET utf8
 COLLATE utf8_general_ci;
 
-DELIMITER $$
-
-CREATE
-DEFINER = 'root'@'localhost'
-TRIGGER BEFORE_INSERT_BALANCE_PER_DAY
-BEFORE INSERT
-ON balance_per_day
-FOR EACH ROW
-BEGIN
-  SET NEW.id = UUID();
-END
-$$
-
-CREATE
-DEFINER = 'root'@'localhost'
-TRIGGER BEFORE_INSERT_subjects
-BEFORE INSERT
-ON subjects
-FOR EACH ROW
-BEGIN
-  SET NEW.id = UUID();
-END
-$$
-
-CREATE
-DEFINER = 'root'@'localhost'
-TRIGGER BEFORE_INSERT_TRANSACTIONS
-BEFORE INSERT
-ON transactions
-FOR EACH ROW
-BEGIN
-  SET NEW.id = UUID();
-END
-$$
-
-
-
-DELIMITER ;
--- db_triggers.sql
+++ db_triggers.sql
@@ -1,37 +1,213 @@
-CREATE TRIGGER on_insert_into_subjects AFTER INSERT ON subjects FOR EACH ROW INSERT INTO Log(id,date,description) values(UUID(),NOW(),CONCAT("new subject ",NEW.id," added in subjects by ",USER()));
 
-CREATE TRIGGER on_insert_into_objects AFTER INSERT ON Objects FOR EACH ROW INSERT INTO Log(id,date,description) values(UUID(),NOW(),CONCAT("new object ",NEW.id," added in Objects by ",USER()));
+DELIMITER $$
+CREATE
+DEFINER = 'root'@'localhost'
+TRIGGER new_sevryukov_task.BEFORE_INSERT_BALANCE_PER_DAY
+BEFORE INSERT
+ON new_sevryukov_task.balance_per_day
+FOR EACH ROW
+BEGIN
+  SET NEW.id = UUID();
+END
+$$
 
-CREATE TRIGGER on_insert_into_assignments AFTER INSERT ON Assignments FOR EACH ROW INSERT INTO Log(id,date,description) values(UUID(),NOW(),CONCAT("new assignment ",NEW.subject_id," - ",NEW.object_id," added in Assignments by ",USER()));
+CREATE
+DEFINER = 'root'@'localhost'
+TRIGGER new_sevryukov_task.BEFORE_INSERT_subjects
+BEFORE INSERT
+ON new_sevryukov_task.subjects
+FOR EACH ROW
+BEGIN
+  SET NEW.id = UUID();
+END
+$$
 
-CREATE TRIGGER on_insert_into_permissions AFTER INSERT ON Permissions FOR EACH ROW INSERT INTO Log(id,date,description) values(UUID(),NOW(),CONCAT("new permission ",NEW.subject_id," : ",NEW.permission," : ",NEW.object_id," added in Permissions by ",USER()));
+CREATE
+DEFINER = 'root'@'localhost'
+TRIGGER new_sevryukov_task.BEFORE_INSERT_TRANSACTIONS
+BEFORE INSERT
+ON new_sevryukov_task.transactions
+FOR EACH ROW
+BEGIN
+  SET NEW.id = UUID();
+END
+$$
 
-CREATE TRIGGER on_insert_into_stages AFTER INSERT ON Stages FOR EACH ROW INSERT INTO Log(id,date,description) values(UUID(),NOW(),CONCAT("new stage ",NEW.id," added in Stages by ",USER()));
+CREATE
+DEFINER = 'root'@'localhost'
+TRIGGER new_sevryukov_task.ins
+AFTER INSERT
+ON new_sevryukov_task.roles
+FOR EACH ROW
+BEGIN
+  IF NEW.role = 'MANAGER' THEN
+    INSERT INTO permissions
+      VALUES (NEW.subject_id, 'DELETE', NEW.object_id), (NEW.subject_id, 'UPDATE', NEW.object_id), (NEW.subject_id, 'READ', NEW.object_id);
+  END IF;
+  IF NEW.role = 'FOREMAN' THEN
+    INSERT INTO permissions
+      VALUES (NEW.subject_id, 'INSERT', NEW.object_id), (NEW.subject_id, 'UPDATE_SELF', NEW.object_id), (NEW.subject_id, 'READ', NEW.object_id);
+  END IF;
+END
+$$
 
-CREATE TRIGGER on_insert_into_elements AFTER INSERT ON Elements FOR EACH ROW INSERT INTO Log(id,date,description) values(UUID(),NOW(),CONCAT("new element ",NEW.id," added in Elements by ",USER()));
+CREATE
+DEFINER = 'root'@'localhost'
+TRIGGER new_sevryukov_task.on_delete_from_assignments
+AFTER DELETE
+ON new_sevryukov_task.assignments
+FOR EACH ROW
+  INSERT INTO Log (id, date, description)
+    VALUES (UUID(), NOW(), CONCAT("assignment ", OLD.subject_id, " - ", OLD.object_id, " deleted from Assignments by ", USER()))
+$$
 
-CREATE TRIGGER on_update_subjects AFTER UPDATE ON subjects FOR EACH ROW INSERT INTO Log(id,date,description) values(UUID(),NOW(),CONCAT("subject ",NEW.id," updated in subjects by ",USER()));
+CREATE
+DEFINER = 'root'@'localhost'
+TRIGGER new_sevryukov_task.on_delete_from_elements
+AFTER DELETE
+ON new_sevryukov_task.elements
+FOR EACH ROW
+  INSERT INTO Log (id, date, description)
+    VALUES (UUID(), NOW(), CONCAT("element ", OLD.id, " deleted from Elements by ", USER()))
+$$
 
-CREATE TRIGGER on_update_objects AFTER UPDATE ON Objects FOR EACH ROW INSERT INTO Log(id,date,description) values(UUID(),NOW(),CONCAT("object ",NEW.id," updated in Objects by ",USER()));
+CREATE
+DEFINER = 'root'@'localhost'
+TRIGGER new_sevryukov_task.on_delete_from_objects
+AFTER DELETE
+ON new_sevryukov_task.objects
+FOR EACH ROW
+  INSERT INTO Log (id, date, description)
+    VALUES (UUID(), NOW(), CONCAT("object ", OLD.id, " deleted from Objectss by ", USER()))
+$$
 
-CREATE TRIGGER on_update_assignments AFTER UPDATE ON Assignments FOR EACH ROW INSERT INTO Log(id,date,description) values(UUID(),NOW(),CONCAT("assignment ",OLD.subject_id," - ",OLD.object_id," updated to ",OLD.subject_id," - ",OLD.object_id, " in Assignments by ",USER()));
+CREATE
+DEFINER = 'root'@'localhost'
+TRIGGER new_sevryukov_task.on_delete_from_permissions
+AFTER DELETE
+ON new_sevryukov_task.permissions
+FOR EACH ROW
+  INSERT INTO Log (id, date, description)
+    VALUES (UUID(), NOW(), CONCAT("permission ", OLD.subject_id, " : ", OLD.permission, " : ", OLD.object_id, " deleted from Permissions by ", USER()))
+$$
 
-CREATE TRIGGER on_update_permissions AFTER UPDATE ON Permissions FOR EACH ROW INSERT INTO Log(id,date,description) values(UUID(),NOW(),CONCAT("permission ",OLD.subject_id," : ",OLD.permission," : ",OLD.object_id, " updated to ",NEW.subject_id," : ",NEW.permission," : ",NEW.object_id," in Permissions by ",USER()));
+CREATE
+DEFINER = 'root'@'localhost'
+TRIGGER new_sevryukov_task.on_delete_from_stages
+AFTER DELETE
+ON new_sevryukov_task.stages
+FOR EACH ROW
+  INSERT INTO Log (id, date, description)
+    VALUES (UUID(), NOW(), CONCAT("stage ", OLD.id, " deleted from Stages by ", USER()))
+$$
 
-CREATE TRIGGER on_delete_from_subjects AFTER DELETE ON subjects FOR EACH ROW INSERT INTO Log(id,date,description) values(UUID(),NOW(),CONCAT("subject ",OLD.id," deleted from subjects by ",USER()));
+CREATE
+DEFINER = 'root'@'localhost'
+TRIGGER new_sevryukov_task.on_delete_from_subjects
+AFTER DELETE
+ON new_sevryukov_task.subjects
+FOR EACH ROW
+  INSERT INTO Log (id, date, description)
+    VALUES (UUID(), NOW(), CONCAT("subject ", OLD.id, " deleted from subjects by ", USER()))
+$$
 
-CREATE TRIGGER on_delete_from_objects AFTER DELETE ON Objects FOR EACH ROW INSERT INTO Log(id,date,description) values(UUID(),NOW(),CONCAT("object ",OLD.id," deleted from Objectss by ",USER()));
+CREATE
+DEFINER = 'root'@'localhost'
+TRIGGER new_sevryukov_task.on_insert_into_assignments
+AFTER INSERT
+ON new_sevryukov_task.assignments
+FOR EACH ROW
+  INSERT INTO Log (id, date, description)
+    VALUES (UUID(), NOW(), CONCAT("new assignment ", NEW.subject_id, " - ", NEW.object_id, " added in Assignments by ", USER()))
+$$
 
-CREATE TRIGGER on_delete_from_assignments AFTER DELETE ON Assignments FOR EACH ROW INSERT INTO Log(id,date,description) values(UUID(),NOW(),CONCAT("assignment ",OLD.subject_id," - ",OLD.object_id," deleted from Assignments by ",USER()));
+CREATE
+DEFINER = 'root'@'localhost'
+TRIGGER new_sevryukov_task.on_insert_into_elements
+AFTER INSERT
+ON new_sevryukov_task.elements
+FOR EACH ROW
+  INSERT INTO Log (id, date, description)
+    VALUES (UUID(), NOW(), CONCAT("new element ", NEW.id, " added in Elements by ", USER()))
+$$
 
-CREATE TRIGGER on_delete_from_permissions AFTER DELETE ON Permissions FOR EACH ROW INSERT INTO Log(id,date,description) values(UUID(),NOW(),CONCAT("permission ",OLD.subject_id," : ",OLD.permission," : ",OLD.object_id, " deleted from Permissions by ",USER()));
+CREATE
+DEFINER = 'root'@'localhost'
+TRIGGER new_sevryukov_task.on_insert_into_objects
+AFTER INSERT
+ON new_sevryukov_task.objects
+FOR EACH ROW
+  INSERT INTO Log (id, date, description)
+    VALUES (UUID(), NOW(), CONCAT("new object ", NEW.id, " added in Objects by ", USER()))
+$$
 
-CREATE TRIGGER on_delete_from_stages AFTER DELETE ON Stages FOR EACH ROW INSERT INTO Log(id,date,description) values(UUID(),NOW(),CONCAT("stage ",OLD.id, " deleted from Stages by ",USER()));
+CREATE
+DEFINER = 'root'@'localhost'
+TRIGGER new_sevryukov_task.on_insert_into_permissions
+AFTER INSERT
+ON new_sevryukov_task.permissions
+FOR EACH ROW
+  INSERT INTO Log (id, date, description)
+    VALUES (UUID(), NOW(), CONCAT("new permission ", NEW.subject_id, " : ", NEW.permission, " : ", NEW.object_id, " added in Permissions by ", USER()))
+$$
 
-CREATE TRIGGER on_delete_from_elements AFTER DELETE ON Elements FOR EACH ROW INSERT INTO Log(id,date,description) values(UUID(),NOW(),CONCAT("element ",OLD.id, " deleted from Elements by ",USER()));
+CREATE
+DEFINER = 'root'@'localhost'
+TRIGGER new_sevryukov_task.on_insert_into_stages
+AFTER INSERT
+ON new_sevryukov_task.stages
+FOR EACH ROW
+  INSERT INTO Log (id, date, description)
+    VALUES (UUID(), NOW(), CONCAT("new stage ", NEW.id, " added in Stages by ", USER()))
+$$
 
+CREATE
+DEFINER = 'root'@'localhost'
+TRIGGER new_sevryukov_task.on_insert_into_subjects
+AFTER INSERT
+ON new_sevryukov_task.subjects
+FOR EACH ROW
+  INSERT INTO Log (id, date, description)
+    VALUES (UUID(), NOW(), CONCAT("new subject ", NEW.id, " added in subjects by ", USER()))
+$$
 
+CREATE
+DEFINER = 'root'@'localhost'
+TRIGGER new_sevryukov_task.on_update_assignments
+AFTER UPDATE
+ON new_sevryukov_task.assignments
+FOR EACH ROW
+  INSERT INTO Log (id, date, description)
+    VALUES (UUID(), NOW(), CONCAT("assignment ", OLD.subject_id, " - ", OLD.object_id, " updated to ", OLD.subject_id, " - ", OLD.object_id, " in Assignments by ", USER()))
+$$
 
+CREATE
+DEFINER = 'root'@'localhost'
+TRIGGER new_sevryukov_task.on_update_objects
+AFTER UPDATE
+ON new_sevryukov_task.objects
+FOR EACH ROW
+  INSERT INTO Log (id, date, description)
+    VALUES (UUID(), NOW(), CONCAT("object ", NEW.id, " updated in Objects by ", USER()))
+$$
 
+CREATE
+DEFINER = 'root'@'localhost'
+TRIGGER new_sevryukov_task.on_update_permissions
+AFTER UPDATE
+ON new_sevryukov_task.permissions
+FOR EACH ROW
+  INSERT INTO Log (id, date, description)
+    VALUES (UUID(), NOW(), CONCAT("permission ", OLD.subject_id, " : ", OLD.permission, " : ", OLD.object_id, " updated to ", NEW.subject_id, " : ", NEW.permission, " : ", NEW.object_id, " in Permissions by ", USER()))
+$$
 
-
+CREATE
+DEFINER = 'root'@'localhost'
+TRIGGER new_sevryukov_task.on_update_subjects
+AFTER UPDATE
+ON new_sevryukov_task.subjects
+FOR EACH ROW
+  INSERT INTO Log (id, date, description)
+    VALUES (UUID(), NOW(), CONCAT("subject ", NEW.id, " updated in subjects by ", USER()))
+$$
+DELIMITER ;
