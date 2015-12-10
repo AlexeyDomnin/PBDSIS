c84448fd02a06c3fb1a239496e41cd3d49208d59
Alexey <aleksei_saharov@bk.ru>
2015-12-10 20:59:06 +0300

    Update db_triggers.sql

--- db_triggers.sql
+++ db_triggers.sql
@@ -8,11 +8,15 @@ CREATE TRIGGER on_insert_into_stages AFTER INSERT ON stages FOR EACH ROW INSERT 
 
 CREATE TRIGGER on_insert_into_elements AFTER INSERT ON elements FOR EACH ROW INSERT INTO log(id,date,description) values(UUID(),NOW(),CONCAT("new element ",NEW.id," added in Elements by ",USER()));
 
+CREATE TRIGGER on_insert_into_tickets AFTER INSERT ON tickets FOR EACH ROW INSERT INTO log(id,date,description) values(UUID(),NOW(),CONCAT("new ticket ",NEW.id," added in Ticketss by ",USER()));
+
 CREATE TRIGGER on_update_entities AFTER UPDATE ON entities FOR EACH ROW INSERT INTO log(id,date,description) values(UUID(),NOW(),CONCAT("entity ",NEW.id," updated in Entities by ",USER()));
 
 CREATE TRIGGER on_update_objects AFTER UPDATE ON objects FOR EACH ROW INSERT INTO log(id,date,description) values(UUID(),NOW(),CONCAT("object ",NEW.id," updated in Objects by ",USER()));
 
 CREATE TRIGGER on_update_permissions AFTER UPDATE ON permissions FOR EACH ROW INSERT INTO log(id,date,description) values(UUID(),NOW(),CONCAT("permission ",OLD.entity_id," : ",OLD.permission," : ",OLD.object_id, " updated to ",NEW.entity_id," : ",NEW.permission," : ",NEW.object_id," in Permissions by ",USER()));
+
+CREATE TRIGGER on_update_tickets AFTER UPDATE ON tickets FOR EACH ROW INSERT INTO log(id,date,description) values(UUID(),NOW(),CONCAT("ticket ",NEW.id," updated in Ticketss by ",USER()));
 
 CREATE TRIGGER on_delete_from_entities AFTER DELETE ON entities FOR EACH ROW INSERT INTO log(id,date,description) values(UUID(),NOW(),CONCAT("entity ",OLD.id," deleted from Entities by ",USER()));
 
@@ -24,6 +28,7 @@ CREATE TRIGGER on_delete_from_stages AFTER DELETE ON stages FOR EACH ROW INSERT 
 
 CREATE TRIGGER on_delete_from_elements AFTER DELETE ON elements FOR EACH ROW INSERT INTO log(id,date,description) values(UUID(),NOW(),CONCAT("element ",OLD.id, " deleted from Elements by ",USER()));
 
+CREATE TRIGGER on_delete_from_tickets AFTER DELETE ON tickets FOR EACH ROW INSERT INTO log(id,date,description) values(UUID(),NOW(),CONCAT("ticket ",OLD.id," deleted from Ticketss by ",USER()));
 
 
 
